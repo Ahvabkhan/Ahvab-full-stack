da42a2c00942c352795e5443523ad64df62491cf
Ahvabkhan <114853694+Ahvabkhan@users.noreply.github.com>
2025-08-21 15:36:03 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -4,7 +4,7 @@ import order_icon from './order_icon.png'
 import upload_area from './upload_area.png'
 import parcel_icon from './parcel_icon.svg'
 
-export const url = "https://ahvab-full-stack-4y3adc2sm-ahvabkhans-projects.vercel.app"
+export const url = "https://ahvab-full-stack.vercel.app/"
 export const assets = {
     logo,
     add_icon,
